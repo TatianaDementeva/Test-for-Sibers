abdfedffef3a0b705a147a46f79951ace80dc23a
Tatiana Dementeva <Gitli@mail.ru>
2020-11-03 06:56:06 +0700

    Moved component app.

--- src/components/app/App.jsx
+++ src/components/app/App.jsx
@@ -1,26 +1,36 @@
 import React, {Component} from "react"; 
 import axios from "axios";
 import './App.css';
-import ContactList from './components/contact-list/contact-list';
+import ContactList from '../contact-list/contact-list';
 
 export default class App extends Component {
-  componentDidMount(){
+  state = {
+      loaded: false
+  }
+
+  componentDidMount() {
     if (localStorage.getItem('loaded') === null){
       axios.get('http://demo.sibers.com/users').then(result => {
         localStorage.setItem('users', JSON.stringify(result.data));
         localStorage.setItem('loaded', 'true');
-            });
+        this.setState({loaded: true});
+      });
     }
-    //delete after test
-    console.log(JSON.parse(localStorage.getItem('users')));
-  };
+    if (localStorage.getItem('loaded') === 'true'){
+      this.setState({loaded: true});
+    }
+  }
+
   render(){
+    const loaded = this.state.loaded;
     return (
       <div className="App">
         <header className="App-header">
           Contact book
         </header>
+        {loaded &&
         <ContactList />
+        }
       </div>
     );
   }
